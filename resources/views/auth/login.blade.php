f8923df62136d7663895cf82f7221dd3b7c44ea9
carlos santos <rickhenriquetab@gmail.com>
2025-01-11 22:19:34 -0300

    removendo todos os darkmode do sistema

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -22,14 +22,14 @@
 
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Lembre de Mim') }}</span>
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Lembre de Mim') }}</span>
             </label>
         </div>
 
         <div class="flex items-center justify-end mt-4">
 
-            <a class="underline text-sm text-gray-900 hover:text-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+            <a class="underline text-sm text-gray-900 hover:text-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
                 {{ __('Não tem uma conta?') }}
             </a>
 
